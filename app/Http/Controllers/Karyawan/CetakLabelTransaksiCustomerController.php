781d8337d5378793f38e1452027fd4352c155ffe
Hade Mopilie <hademopilie@gmail.com>
2022-12-08 19:30:25 +0800

    feat: change role

--- app/Http/Controllers/Karyawan/CetakLabelTransaksiCustomerController.php
+++ app/Http/Controllers/Karyawan/CetakLabelTransaksiCustomerController.php
@@ -6,34 +6,25 @@ use App\Http\Controllers\Controller;
 use App\Models\transaksi;
 use Illuminate\Http\Request;
 use clsTinyButStrong;
+use clsOpenTBS;
 use PhpOffice\PhpWord\TemplateProcessor;
 
 class CetakLabelTransaksiCustomerController extends Controller
 {
     function request(Request $request)
     {
+//
+//        $templateProcessor = new TemplateProcessor($path);
+//        $templateProcessor->setValues([
+//            'laundry_nama' => 'Babussalam Laundry',
+//            'nama' => $invoice->customers->name,
 
-        $invoice = transaksi::with('price')->with('customers')
-            ->where('id', $request->transaksi_id)
-            ->first();
-        $path = storage_path('app/template/CetakLabelCucianFormat1.docx');
-        $content = base64_encode( 'CetakThermal|' . $request->url . '|CetakThermal|1|');
+//        ]);
+//
+//        $output = storage_path('app/output/CetakLabelCucianFormat_'.$invoice->id.'.docx');
+//        $templateProcessor->saveAs($output);
 
-        $templateProcessor = new TemplateProcessor($path);
-        $templateProcessor->setValues([
-            'laundry_nama' => 'Babussalam Laundry',
-            'nama' => $invoice->customers->name,
-            'alamat' => $invoice->customers->alamat,
-            'no_telp' => $invoice->customers->no_telp,
-            'tanggal_masuk' => $invoice->created_at,
-            'jenis_layanan' => $invoice->price->jenis,
-            'berat' => $invoice->kg,
-            'harga' => $invoice->harga_akhir,
-        ]);
-
-        $output = storage_path('app/output/CetakLabelCucianFormat_'.$invoice->id.'.docx');
-        $templateProcessor->saveAs($output);
-
+        $content = base64_encode('CetakThermal|' . $request->url . '|CetakThermal|1|');
         $result = [
             "status" => 200,
             "service" => "printerservices",
@@ -47,15 +38,28 @@ class CetakLabelTransaksiCustomerController extends Controller
 
     function index($transaksi)
     {
-        $output = storage_path('app/output/CetakLabelCucianFormat_'.$transaksi.'.docx');
+        $path = storage_path('app/template/CetakLabelCucianFormat1.docx');
+        $invoice = transaksi::with('price')->with('customers')
+            ->where('id', $transaksi)
+            ->first();
+
         $TBS = new clsTinyButStrong();
-        $TBS->LoadTemplate($output);
-        $TBS->Show(32);
-        $content = base64_encode($TBS->Source);
+        $TBS->Plugin(TBS_INSTALL, 'clsOpenTBS');
+        $TBS->LoadTemplate($path, OPENTBS_ALREADY_UTF8);
+        $TBS->VarRef['laundry_nama'] = 'Babussalam Laundry';
+        $TBS->VarRef['nama'] = $invoice->customers->name;
+        $TBS->VarRef['alamat'] = $invoice->customers->alamat;
+        $TBS->VarRef['no_telp'] = $invoice->customers->no_telp;
+        $TBS->VarRef['tanggal_masuk'] = $invoice->created_at;
+        $TBS->VarRef['jenis_layanan'] = $invoice->price->jenis;
+        $TBS->VarRef['berat'] = $invoice->kg;
+        $TBS->VarRef['harga'] = $invoice->harga_akhir;
+        $TBS->Show(OPENTBS_STRING);
+        $reports = base64_encode($TBS->Source);
 
         return response()->json([
             "status" => 200,
-            "content" => $content
+            "content" => $reports
         ]);
     }
 }
